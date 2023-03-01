97f5630bb09b421ab28d85c582b74c7634688214
Doheera-kosi <kupourevans99@gmail.com>
2023-03-01 01:03:24 +0000

    Fix lint errors

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,5 +1,5 @@
 class ApiController < ApplicationController
   def random_greeting
-    render json: { greeting: Message.order("RANDOM()").first.greeting }
+    render json: { greeting: Message.order('RANDOM()').first.greeting }
   end
 end
